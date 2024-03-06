7ba4cee2341e337864685635d267a9d3c932fca5
alteredtech <michael@einreinhof.com>
2024-03-06 05:12:33 +0000

    Another Tools release v1.2.33

--- Formula/random-number.rb
+++ Formula/random-number.rb
@@ -5,12 +5,13 @@ class RandomNumber < Formula
     include Language::Python::Virtualenv
     desc "test repo for trying homebrew packaging"
     homepage "https://github.com/alteredtech/random-number"
-    
-    version v1.2.22
+
+    release = JSON.parse(File.open(File.expand_path('../../tools/release.json', __FILE__)).read)
+    version release['version']
     license "MIT"
 
-    url "https://github.com/alteredtech/random-number/releases/download/v1.2.22/random-number-release.tar.gz"
-    sha256 
+    url "https://github.com/alteredtech/random-number/releases/download/#{version}/random-number-release.tar.gz"
+    sha256 release['sha256']
     
     depends_on "python@3.12"
 
