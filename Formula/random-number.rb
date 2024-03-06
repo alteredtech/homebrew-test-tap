4b9057b7f012dac80a16750dd8ae209701c37311
alteredtech <michael@einreinhof.com>
2024-03-06 05:18:19 +0000

    Another Tools release v1.2.34

--- Formula/random-number.rb
+++ Formula/random-number.rb
@@ -16,6 +16,101 @@ class RandomNumber < Formula
     depends_on "python@3.12"
 
     
+    resource blessed do
+        url https://files.pythonhosted.org/packages/25/ae/92e9968ad23205389ec6bd82e2d4fca3817f1cdef34e10aa8d529ef8b1d7/blessed-1.20.0.tar.gz
+        sha256 2cdd67f8746e048f00df47a2880f4d6acbcdb399031b604e34ba8f71d5787680
+    end
+    
+    resource boto3 do
+        url https://files.pythonhosted.org/packages/3e/b8/224ef51256b26ea79909ac1c432be8711ed7b1f6f6b73508d905facd2fae/boto3-1.34.56.tar.gz
+        sha256 b26928f9a21cf3649cea20a59061340f3294c6e7785ceb6e1a953eb8010dc3ba
+    end
+    
+    resource botocore do
+        url https://files.pythonhosted.org/packages/6a/c6/9c48501759b6ceb726ad94c0a13372c9d8442c8df7e0de424024bc285d78/botocore-1.34.56.tar.gz
+        sha256 bffeb71ab21d47d4ecf947d9bdb2fbd1b0bbd0c27742cea7cf0b77b701c41d9f
+    end
+    
+    resource certifi do
+        url https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2024.2.2.tar.gz
+        sha256 0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f
+    end
+    
+    resource charset-normalizer do
+        url https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz
+        sha256 f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5
+    end
+    
+    resource editor do
+        url https://files.pythonhosted.org/packages/2a/92/734a4ab345914259cb6146fd36512608ea42be16195375c379046f33283d/editor-1.6.6.tar.gz
+        sha256 bb6989e872638cd119db9a4fce284cd8e13c553886a1c044c6b8d8a160c871f8
+    end
+    
+    resource idna do
+        url https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz
+        sha256 9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca
+    end
+    
+    resource inquirer do
+        url https://files.pythonhosted.org/packages/03/eb/b631f7ed6156717cd5bd9abc8c9df809fe128a389a0377274f2d6cb102db/inquirer-3.2.4.tar.gz
+        sha256 33b09efc1b742b9d687b540296a8b6a3f773399673321fcc2ab0eb4c109bf9b5
+    end
+    
+    resource jmespath do
+        url https://files.pythonhosted.org/packages/00/2a/e867e8531cf3e36b41201936b7fa7ba7b5702dbef42922193f05c8976cd6/jmespath-1.0.1.tar.gz
+        sha256 90261b206d6defd58fdd5e85f478bf633a2901798906be2ad389150c5c60edbe
+    end
+    
+    resource python-dateutil do
+        url https://files.pythonhosted.org/packages/66/c0/0c8b6ad9f17a802ee498c46e004a0eb49bc148f2fd230864601a86dcf6db/python-dateutil-2.9.0.post0.tar.gz
+        sha256 37dd54208da7e1cd875388217d5e00ebd4179249f90fb72437e91a35459a0ad3
+    end
+    
+    resource readchar do
+        url https://files.pythonhosted.org/packages/a1/57/439aaa28659e66265518232bf4291ae5568aa01cd9e0e0f6f8fe3b300e9e/readchar-4.0.5.tar.gz
+        sha256 08a456c2d7c1888cde3f4688b542621b676eb38cd6cfed7eb6cb2e2905ddc826
+    end
+    
+    resource requests do
+        url https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz
+        sha256 942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1
+    end
+    
+    resource runs do
+        url https://files.pythonhosted.org/packages/26/6d/b9aace390f62db5d7d2c77eafce3d42774f27f1829d24fa9b6f598b3ef71/runs-1.2.2.tar.gz
+        sha256 9dc1815e2895cfb3a48317b173b9f1eac9ba5549b36a847b5cc60c3bf82ecef1
+    end
+    
+    resource s3transfer do
+        url https://files.pythonhosted.org/packages/a0/b5/4c570b08cb85fdcc65037b5229e00412583bb38d974efecb7ec3495f40ba/s3transfer-0.10.0.tar.gz
+        sha256 d0c8bbf672d5eebbe4e57945e23b972d963f07d82f661cabf678a5c88831595b
+    end
+    
+    resource setuptools do
+        url https://files.pythonhosted.org/packages/c8/1f/e026746e5885a83e1af99002ae63650b7c577af5c424d4c27edcf729ab44/setuptools-69.1.1.tar.gz
+        sha256 5c0806c7d9af348e6dd3777b4f4dbb42c7ad85b190104837488eab9a7c945cf8
+    end
+    
+    resource six do
+        url https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz
+        sha256 1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926
+    end
+    
+    resource urllib3 do
+        url https://files.pythonhosted.org/packages/af/47/b215df9f71b4fdba1025fc05a77db2ad243fa0926755a52c5e71659f4e3c/urllib3-2.0.7.tar.gz
+        sha256 c97dfde1f7bd43a71c8d2a58e369e9b2bf692d1334ea9f9cae55add7d0dd0f84
+    end
+    
+    resource wcwidth do
+        url https://files.pythonhosted.org/packages/6c/63/53559446a878410fc5a5974feb13d31d78d752eb18aeba59c7fef1af7598/wcwidth-0.2.13.tar.gz
+        sha256 72ea0c06399eb286d978fdedb6923a9eb47e1c486ce63e9b4e64fc18303972b5
+    end
+    
+    resource xmod do
+        url https://files.pythonhosted.org/packages/72/b2/e3edc608823348e628a919e1d7129e641997afadd946febdd704aecc5881/xmod-1.8.1.tar.gz
+        sha256 38c76486b9d672c546d57d8035df0beb7f4a9b088bc3fb2de5431ae821444377
+    end
+    
   
     def install
       Dir["scripts/*.py"].each do |file|
