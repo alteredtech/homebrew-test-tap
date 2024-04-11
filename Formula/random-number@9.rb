fea6c5d8ae8802c2d7d2ad05bd3aceadc9c3b0e2
alteredtech <michael@einreinhof.com>
2024-04-11 07:00:00 +0000

    Another Tools release 58/merge

--- Formula/random-number@9.rb
+++ Formula/random-number@9.rb
@@ -41,8 +41,8 @@ class RandomNumberAT9 < Formula
     end
     
     resource "idna" do
-        url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
-        sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
+        url "https://files.pythonhosted.org/packages/21/ed/f86a79a07470cb07819390452f178b3bef1d375f2ec021ecfc709fc7cf07/idna-3.7.tar.gz"
+        sha256 "028ff3aadf0609c1fd278d8ea3089299412a7a8b9bd005dd08b9f8285bcb5cfc"
     end
     
     resource "jira" do
@@ -60,9 +60,9 @@ class RandomNumberAT9 < Formula
         sha256 "eb82c5e3e56209074766e6885bb04b8c38a0c015d0a30036ebe7ece34c9989e9"
     end
     
-    resource "Pillow" do
-        url "https://files.pythonhosted.org/packages/73/59/3192bb3bc554ccbd678bdb32993928cb566dccf32f65dac65ac7e89eb311/Pillow-8.1.0.tar.gz"
-        sha256 "887668e792b7edbfb1d3c9d8b5d8c859269a0f0eba4dda562adb95500f60dbba"
+    resource "pillow" do
+        url "https://files.pythonhosted.org/packages/ef/43/c50c17c5f7d438e836c169e343695534c38c77f60e7c90389bd77981bc21/pillow-10.3.0.tar.gz"
+        sha256 "9d2455fbf44c914840c793e89aa82d0e1763a14253a000743719ae5946814b2d"
     end
     
     resource "requests" do
