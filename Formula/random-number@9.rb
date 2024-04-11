230d9d42814c9e543f2ef1264f22dbfde9cc2938
Michael Einreinhof <61751327+alteredtech@users.noreply.github.com>
2024-04-10 22:04:04 -0600

    Update random-number@9.rb

--- Formula/random-number@9.rb
+++ Formula/random-number@9.rb
@@ -100,9 +100,26 @@ class RandomNumberAT9 < Formula
   
     def install
         venv = virtualenv_create(libexec, python3)
-        resource("Pillow").stage { system "python", *Language::Python.setup_install_args(libexec/"vendor") }
+        resource("Pillow").stage do
+          inreplace "setup.py" do |s|
+            sdkprefix = MacOS.sdk_path_if_needed ? MacOS.sdk_path : ""
+            s.gsub! "openjpeg.h", "probably_not_a_header_called_this_eh.h"
+            s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{sdkprefix}/usr/lib', '#{sdkprefix}/usr/include')"
+            s.gsub! "JPEG_ROOT = None", "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
+            s.gsub! "FREETYPE_ROOT = None", "FREETYPE_ROOT = ('#{Formula["freetype"].opt_prefix}/lib', '#{Formula["freetype"].opt_prefix}/include')"
+          end
+    
+          # avoid triggering "helpful" distutils code that doesn't recognize Xcode 7 .tbd stubs
+          ENV.append "CFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers" unless MacOS::CLT.installed?
+          venv.pip_install Pathname.pwd
+        end
+        res = resources.map(&:name).to_set - ["Pillow"]
+        res.each do |r|
+          venv.pip_install resource(r)
+        end
+        
+        # venv.pip_install resources
         venv.pip_install resources.reject { |r| r.name == "Pillow" }
-        venv.pip_install resources
         venv.pip_install_and_link buildpath
     end
   
